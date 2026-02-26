222bb53f856f88aa19264d0738828df78185fcda
yasua006 <156091445+yasua006@users.noreply.github.com>
2026-02-26 09:57:40 +0100

    Lag førrige resultat & UI og andre forbedring
    Lagde førrige resultat ved bruk av `localStorage`
    Mer UI spacing - ikke rottede
    
    Ved et uhell feil rett
    Lagde oppdaterte target - glemte å gjøre det
    
    Andre
    Bedre feilmeldinger - hvilke data og hva er -
    ikke ok
    
    Form tag er mer lesbart - Vi bruker bare GET

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const storage_key = "Previous conversion result";
     const convert_curr = async (base, target, amount) => {
         const EXCHANGE_RATE_API_KEY = "c3bebc3cf92257a19192d324";
         const res = await fetch(`https://v6.exchangerate-api.com/v6/${EXCHANGE_RATE_API_KEY}/pair/${base}/${target}/${amount}`);
         if (!res.ok) {
-            throw new Error("Not ok!");
+            throw new Error("Bad API response!");
         }
         const data = await res.json();
         if (!data) {
-            throw new Error("No data!");
+            throw new Error("No JSON data!");
         }
         return data.conversion_result;
     };
-    const { createApp, ref, onMounted } = Vue;
+    const { createApp, ref } = Vue;
     const app = createApp({
         setup() {
-            const base = ref("USD (Amerikanske Dollar) 🇺🇸");
-            const target = ref("EUR (Euro) 🇪🇺");
+            const base = ref("");
+            const target = ref("");
             const amount = ref(1000);
             const result = ref(null);
+            const previous_result = ref("Førrige resultat: " + localStorage.getItem(storage_key));
             const convert = async () => {
                 try {
-                    result.value = await convert_curr(base.value, target.value, amount.value);
+                    result.value = "Resultat: " + await convert_curr(base.value, target.value, amount.value);
+                    // console.log(base);
+                    // console.log(base.value);
+                    // console.log(target);
+                    // console.log(target.value);
+                    localStorage.removeItem(storage_key);
+                    localStorage.setItem(storage_key, result.value);
                 }
                 catch (err) {
                     throw new Error(`Can't convert! ${err}`);
                 }
             };
-            onMounted(convert);
             return {
                 base,
                 target,
                 amount,
                 result,
+                previous_result,
                 convert
             };
         }
